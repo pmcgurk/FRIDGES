980c42aea930e7bcaf926b01d8768d5136f50841
Paul McGurk <pmcgurk@users.noreply.github.com>
2015-10-10 00:30:52 +0100

    RGB LED program now works with breadboard

--- Prototypes/TempSensLED/changeLED.py
+++ Prototypes/TempSensLED/changeLED.py
@@ -1,4 +1,24 @@
 # Program that changes the LED by modifying the GPIO
+import RPi.GPIO as GPIO
+import time
+
+# Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+# Start Pulse Width Modulation (PWM) on the red, green and blue channels to 
+# control the brightness of the LEDs.
+# Follow this link for more info on PWM: http://en.wikipedia.org/wiki/Pulse-width_modulation
+pwmRed = GPIO.PWM(8, 500)
+pwmRed.start(100)
+
+pwmGreen = GPIO.PWM(13, 500)
+pwmGreen.start(100)
+
+pwmBlue = GPIO.PWM(16, 500)
+pwmBlue.start(100)
 
 def changeLED(RGB):
     changeRed(RGB[0])
@@ -6,16 +26,13 @@ def changeLED(RGB):
     changeBlue(RGB[2])
 
 def changeGreen(value):
-    #print ('[changeLED] Changed Green to value: ' + str(value));
-    dummyMethodBecausePythonDoesntLikeEmptyIfStatements()
+	#print ('[changeLED] Changed Green to value: ' + str(value));
+	pwmGreen.ChangeDutyCycle(float(value))
 
 def changeBlue(value):
-    #print ('[changeLED] Changed Blue to value: ' + str(value));
-    dummyMethodBecausePythonDoesntLikeEmptyIfStatements()
+	#print ('[changeLED] Changed Blue to value: ' + str(value));
+	pwmBlue.ChangeDutyCycle(float(value))
 
 def changeRed(value):
-    #print ('[changeLED] Changed Red to value: ' + str(value));
-    dummyMethodBecausePythonDoesntLikeEmptyIfStatements()
-
-def dummyMethodBecausePythonDoesntLikeEmptyIfStatements():
-    a = 1
+	#print ('[changeLED] Changed Red to value: ' + str(value));
+	pwmRed.ChangeDutyCycle(float(value))
--- Prototypes/TempSensLED/main.py
+++ Prototypes/TempSensLED/main.py
@@ -3,14 +3,18 @@ import LEDColour
 import temperature
 import changeLED
 import time
+import RPi.GPIO as GPIO
 
 def main():
+    LEDColour.test()
     LEDColour.setup(0, 10, True)
+    temp = -1.0
     while (True):
-        temp = temperature.getTemperature()
+        temp = temp + 0.0001 # test value as get temperature doesnt work
+        #temp = temperature.getTemperature()
         RGB = LEDColour.getLEDColour(temp)
         changeLED.changeLED(RGB)
-        time.sleep(1)
+        time.sleep(0.0001)
+			
 
-#LEDColour.test()
 main()
