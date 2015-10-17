39d1fe2cfbe7b3424fe55fbb6e68e3760c4bd2b4
Paul McGurk <pmcgurk@users.noreply.github.com>
2015-10-17 13:37:31 +0100

    Start of touchscreen GUI
    modals are there but are not fully working

---
+++ Prototypes/touchscreenInterface/js/js.js
@@ -0,0 +1,7 @@
+  $(document).ready(function () {
+      $('.button-collapse').sideNav({
+          menuWidth: 175,
+          closeOnClick: true
+      });
+      $('.modal-trigger').leanModal();
+  });
