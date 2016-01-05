88b6051ad2e656d93b2d649a9d1006d3555b3159
paul mcgurk <dsb12187@uni.strath.ac.uk>
2016-01-05 14:14:01 +0000

    flask database

---
+++ Prototypes/Database/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, send_from_directory
+import datetime
+from flaskext.mysql import MySQL
+app = Flask(__name__)
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'FRIDGES'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route('/')
+def mypisite(name=None):
+	return render_template('index.html')
+
+@app.route('/SQL')
+def SQL():
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from Products")
+	data = cursor.fetchone()
+	if data is None:
+		return "Logged in wrong"
+	else:
+		return str(data)
+
+if __name__ == "__main__":
+
+	app.run('0.0.0.0')
+
---
+++ Prototypes/Database/static/python/main.py
@@ -0,0 +1,28 @@
+## main program ##
+
+## gets latest data from all the python programs, database ##
+## updates LED, parses into JSON object ##
+
+## ie data = {temp: 12.2, doorOpen: false, products: {name: "Sandwich", date:"01/01/01"}} etc##
+
+# simple main for the temperature sensitive LED prototype
+import LEDColour, temperature, changeLED, time, doorStatus, RPi.GPIO as GPIO, json
+from time import gmtime, strftime
+
+
+def main():
+    target = 0;
+    maxTemp = 10
+    LEDColour.setup(target, maxTemp, True)
+    while (True):
+        temp = round(temperature.getTemperature(), 1) #get temperature
+        doorOpen = doorStatus.getDoorStatus(); # change to hall effect result
+        changeLED.changeLED(LEDColour.getLEDColour(temp))
+        time.sleep(1)
+        updateTime = strftime("%Y-%m-%d %H:%M:%S", gmtime());
+        with open('../../data.json', 'w') as outfile:
+            json.dump({'time': updateTime, 'temp': temp, 'target': target, 'doorOpen': doorOpen, 
+                       'products': [    {'pid': 1, 'name': "sandwich", 'date': "14-01-2016"},
+                                        {'pid': 2, 'name': "cucumber", 'date': "15-01-2016"},]
+                        }, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+main()
