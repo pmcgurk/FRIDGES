fc011d76d37ddf25374902775e5dc54bc23abd5c
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-01-06 20:04:17 +0000

    Changes to javascript and html
    Removed redundant code
    Got ready for database stuff (remove, edit, add)

--- app/local/js/js.js
+++ app/local/js/js.js
@@ -17,6 +17,9 @@
       $('.modal-trigger').leanModal();
 
       $('.modal-opener').click(modalOpenFunc);
+
+      $('#addButton').click(addItem);
+      $('#removeButton').click(removeItem);
       interval = setInterval(update, 2000);
   });
 
@@ -28,8 +31,20 @@
       $.getJSON("../data.json", updateData);
   }
 
-  function dismissItem(data) {
-      console.log("Remove item PID: " + data);
+  function addItem() {
+      Materialize.toast("Display Modal with Forms to add item, then add item to database", 2000);
+  }
+
+  function removeItem() {
+      Materialize.toast("Display Modal with list of items, allow user to click, then remove item from database", 2000);
+  }
+
+  function removeThisItem(data) {
+      Materialize.toast("Remove PID: " + data, 2000);
+  }
+
+  function editItem(data) {
+      Materialize.toast("Display Modal with Forms to edit item, i.e. to add expiry date", 2000);
   }
 
 
@@ -46,7 +61,7 @@
       //console.log(data);
       var table = "<table><thead><tr><th>Product</th><th>Date</th></tr></thead><tbody>";
       for (var i = 0; i < data.length; i++) {
-          table = table + "<tr><td>" + data[i].name + "</td><td>" + data[i].date + "</td><td><a class='red-text' onclick='dismissItem(" + data[i].pid + ")'>X</a></td></tr>";
+          table = table + "<tr><td>" + data[i].name + "</td><td>" + data[i].date + "</td><td><a class='white-text' onclick='editItem(" + data[i].pid + ")'>Edit</a></td><td><a class='red-text' onclick='removeThisItem(" + data[i].pid + ")'>Remove</a></td></tr>";
       }
       table = table + "</tbody></table>";
       $('#productDisplay').html(table);
